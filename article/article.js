45779e3bf5c1e39a25aaab0efb2127a8dbb17c43
FionaBourgaux <145323668+FionaBourgaux@users.noreply.github.com>
2023-10-02 11:22:29 +0200

    Initial commit

---
+++ article/article.js
@@ -0,0 +1,41 @@
+import * as bootstrap from 'bootstrap';
+import '../style.scss';
+import { data } from '../data';
+import { nav } from '../nav';
+
+const detailsarticle = () => {
+  // récupération des paramètres GET de l'url
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const articleId = urlParams.get('id') ?? 1;
+  console.log(articleId);
+
+  // on cherche la article qui possède l'id trouvé dans l'url
+  const article = data.find((article) => {
+    return article.id === Number.parseInt(articleId);
+  });
+  console.log(article);
+
+  return `
+      <div class="card col col-sm-10 col-md-8 col-xl-6 mx-auto">
+        <img src="${article.avatar}" class="card-img-top" alt="avatar de ${article.titre}">
+        <div class="card-body">
+          <h5 class="card-title">${article.titre}</h5>
+          <p class="card-text">
+          ${article.contenue}
+          </p>
+          
+        </div>
+      </div>
+    `;
+};
+
+document.querySelector('#app').innerHTML = `
+  <main>
+    ${nav}
+
+    <div class="container-fluid my-4">
+      ${detailsarticle()}
+    </div>
+  </main>
+`;
---
+++ footer.js
@@ -0,0 +1,30 @@
+export const footer = ` <footer class="bg-dark text-white">
+<div class="container py-4">
+    <div class="row">
+        <div class="col-lg-4">
+            <h4>À propos</h4>
+            <p>Votre description ici.</p>
+        </div>
+        <div class="col-lg-4">
+            <h4>Contactez-nous</h4>
+            <address>
+                <p>Adresse : 123 Rue de la Rue</p>
+                <p>Email : contact@monsite.com</p>
+                <p>Téléphone : +1 (123) 456-7890</p>
+            </address>
+        </div>
+        <div class="col-lg-4">
+            <h4>Suivez-nous</h4>
+            <ul class="list-unstyled">
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Instagram</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="text-center py-3" id="copyright">
+    &copy; 2023 Mon Site Web. Tous droits réservés.
+</div>
+</footer>
+`
